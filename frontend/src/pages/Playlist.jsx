5bcd8d741566ace71b7f9cf063eb7bdaa327e6d5
sumitiitj493-boop <sumitiitj493@gmail.com>
2026-05-08 10:06:42 +0300

    Add Playlist page stub to fix build

---
+++ frontend/src/pages/Playlist.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function PlaylistPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Playlist</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
